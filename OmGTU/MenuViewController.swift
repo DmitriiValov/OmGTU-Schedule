9b54e25c8b17e54f1e624feb0fcea9598de7939e
Dmitry Valov <d_valov@mail.ru>
2018-03-05 23:16:41 +0600

    Version prepeared for uploading.

--- OmGTU/MenuViewController.swift
+++ OmGTU/MenuViewController.swift
@@ -10,6 +10,18 @@ import UIKit
 
 class MenuViewController: UIViewController {
 
+    
+    @IBAction func openSiteAction(_ sender: UIButton) {
+        guard let url = URL(string: "http://komnata13.ru/?p=omgtu") else {
+            return //be safe
+        }
+        if #available(iOS 10.0, *) {
+            UIApplication.shared.open(url, options: [:], completionHandler: nil)
+        } else {
+            UIApplication.shared.openURL(url)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
